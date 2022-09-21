65a869a9571ca5a212ff6b483621aa71d96b0203
tito-arch <bunniestore.rabbitgroup@gmail.com>
2022-09-21 13:22:14 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import Home from './pages/Home';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    // BEM
+ 
+    <Router>
+     <div className="app">
+       <Routes>
+         
+        <Route path="search" element={<h1>This is the search paaaage!</h1>}/>
+        <Route path="/" element={<Home />} />
+
+       </Routes>
+       </div>
+    </Router>
+
   );
 }
 
---
+++ src/Components/Search.js
@@ -0,0 +1,47 @@
+    import React, { useState } from 'react';
+    import './search.css'
+    import MicIcon from '@material-ui/icons/Mic';
+    import SearchIcon from '@material-ui/icons/Search';
+    import Button from '@material-ui/core/Button';
+    import {useNavigate} from 'react-router-dom';
+
+    function Search({hideButtons = false}) {
+        const[input, setInput] = useState('');
+        const navigate = useNavigate();
+        
+        const searchPage = (e) => {
+            e.preventDefault();
+
+            console.log('You hit the search button >>', input);
+
+            //do something with input.....come back to this
+            
+            navigate('/search');
+        }
+    return (
+        <form className='search'>
+        <div className='search__input'>
+         
+         <SearchIcon  className="search__inputIcon"/>
+         <input value={input} onChange={e => setInput(e.target.value)}/>
+         <MicIcon />
+
+        </div> 
+      
+      {!hideButtons &&(
+      
+      <div className='search__buttons'>
+            <Button type='submit' onClick={searchPage} variant="outlined">Google Search</Button>
+            <Button variant="outlined">I'm Feeling Lucky</Button>
+        </div>
+        
+      
+        
+        )}
+        
+      
+        </form>
+    )
+    }
+
+    export default Search
